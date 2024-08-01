7cca64e1c6adccff6e5d8b9757243077aa1cf5e2
Luke Chen <heylukechen@gmail.com>
2024-08-01 22:06:13 +0200

    Tailwind tweak

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { useEffect, useState } from "react";
 // import viteLogo from '/vite.svg'
 import "./App.css";
 
+
+
 function App() {
   const [articleTitle, setArticleTitle] = useState("");
   const [tableOfContent, setTableOfContent] = useState("");
@@ -32,19 +34,19 @@ function App() {
 
   return (
     <div className="flex">
-      <div className="flex flex-col gap-4 mr-10">
-        <h1 className="">{articleTitle}</h1>
-        <hr className="bg-yellow-300 h-3 border-none" />
+      <div className="flex flex-col gap-4 mr-40">
+        <h1 className="mb-12">{articleTitle}</h1>
+        <hr className="bg-yellow-300 h-12 border-none mb-12" />
         <div
-          className="flex flex-col"
+          className="flex flex-col pr-64" id="bodyContent"
           dangerouslySetInnerHTML={{ __html: articleBodyContent }}
         />{" "}
       </div>
       <div className="sticky top-2 self-start">
-        <h3>Introduction</h3>
+        <h3 className="mb-16">Introduction</h3>
         <hr className="bg-slate-500 h-0.5 border-none" />
         <div
-          className="tableOfContent"
+          className="tableOfContent mt-5"
           dangerouslySetInnerHTML={{ __html: tableOfContent }}
         />{" "}
       </div>
